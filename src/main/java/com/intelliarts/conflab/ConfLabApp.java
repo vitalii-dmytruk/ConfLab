2df7f6483f9cd9e147e342ff28deec8b6b9d5a9f
vitalii-dmytruk <vdmytruk@intelliarts.com>
2015-11-09 15:18:37 +0200

    initial app

---
+++ src/main/java/com/intelliarts/conflab/ConfLabApp.java
@@ -0,0 +1,11 @@
+package com.intelliarts.conflab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfLabApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfLabApp.class, args);
+    }
+}
