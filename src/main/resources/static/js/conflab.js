da73e1ed885a6891ac68b48a6189336ba699b965
vitalii-dmytruk <vdmytruk@intelliarts.com>
2015-11-10 10:56:57 +0200

    add requirejs on client

---
+++ src/main/resources/static/js/conflab.js
@@ -0,0 +1,10 @@
+require([   "jquery"
+        ], function () {
+
+    'use strict';
+
+    $(function () {
+
+    });
+
+});
---
+++ src/main/resources/static/js/require.config.js
@@ -0,0 +1,49 @@
+var require = {
+    baseUrl: '../',
+    paths  : {
+        "core"               : 'js/core',
+        "common"             : 'js/common',
+        "menu"               : 'js/menu',
+        "header"             : 'js/header',
+        "profile"            : 'js/profile',
+        "dashboard"          : 'js/dashboard',
+        "text"               : 'lib/requirejs-text/text',
+        "jquery"             : 'lib/jquery/jquery.min',
+        "select2"            : 'lib/select2/select2',
+        "underscore"         : 'lib/underscore/underscore-min',
+        "backbone"           : 'lib/backbone/backbone-min',
+        "backbone.marionette": 'lib/marionette/backbone.marionette',
+        "backbone.babysitter": 'lib/backbone.babysitter/backbone.babysitter',
+        "backbone.radio"     : 'lib/backbone.radio/backbone.radio',
+        "backbone.stickit"   : 'lib/backbone.stickit/backbone.stickit',
+        "bootstrap"          : 'lib/bootstrap/js/bootstrap.min'
+    },
+    shim   : {
+        "jquery"             : {
+            exports: "$"
+        },
+        "select2"            : {
+            "deps": ['jquery']
+        },
+        "bootstrap"          : {
+            "deps": ['jquery']
+        },
+        "underscore"         : {
+            "exports": "_"
+        },
+        "backbone"           : {
+            "deps"   : ['underscore'],
+            "exports": "Backbone"
+        },
+        "backbone.marionette": {
+            "deps"   : ['backbone', 'backbone.babysitter', 'backbone.radio'],
+            "exports": "Marionette"
+        },
+        "backbone.stickit"   : {
+            "deps": ['backbone', 'underscore', 'jquery']
+        },
+        "backbone.validation": {
+            "deps": ['backbone', 'underscore']
+        }
+    }
+};
