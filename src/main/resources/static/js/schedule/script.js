adbea0d0e8f9609cfb1f99b010afd9d4091ba144
Roman Isko <iskoroman@gmail.com>
2015-11-16 16:43:49 +0200

    added example with draggable and droppable  HTML elements

---
+++ src/main/resources/static/js/schedule/script.js
@@ -0,0 +1,32 @@
+$(window).load(function () {
+    $(".draggable").draggable({cursor: "crosshair", revert: "invalid"});
+    $("[id*='drop']").droppable({
+        accept: ".draggable",
+        drop: function (event, ui) {
+            console.log("drop");
+            $(this).removeClass("border").removeClass("over");
+            var dropped = ui.draggable;
+            var droppedOn = $(this);
+            $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
+        },
+        over: function (event, elem) {
+            $(this).addClass("over");
+            console.log("over");
+        }
+        ,
+        out: function (event, elem) {
+            $(this).removeClass("over");
+        }
+    });
+    $("[id*='drop']").sortable();
+
+    $("[id$='origin']").droppable({
+        accept: ".draggable", drop: function (event, ui) {
+            console.log("drop");
+            $(this).removeClass("border").removeClass("over");
+            var dropped = ui.draggable;
+            var droppedOn = $(this);
+            $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
+        }
+    });
+});
