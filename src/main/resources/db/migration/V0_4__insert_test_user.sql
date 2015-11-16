546cdd4371d4a99505b8ac757a5b84ac8f0ddae5
omostepaniuk <omostepaniuk@intelliarts.com>
2015-11-16 16:06:06 +0200

    Migration to add test user to users table

---
+++ src/main/resources/db/migration/V0_4__insert_test_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, email, first_name, last_name)
+VALUES (
+    'test', '$2a$10$B4hOiqWZooZfi1Pd.euGZuDmVrE7YNiIii37tYSUwwKFgdbY7qz8y', 'test@test.com', 'John', 'Doe'
+);
