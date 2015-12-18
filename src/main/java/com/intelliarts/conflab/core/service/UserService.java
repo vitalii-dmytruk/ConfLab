57d26ed183363a7273813839dc1b531d1857776c
Igor Rudnyk <irudnyk@intelliarts.com>
2015-12-18 18:26:48 +0200

    [#109794824] resolve rebase conflict

--- src/main/java/com/intelliarts/conflab/core/service/UserService.java
+++ src/main/java/com/intelliarts/conflab/core/service/UserService.java
@@ -1,42 +1,29 @@
 package com.intelliarts.conflab.core.service;
 
-import com.intelliarts.conflab.api.User;
-import com.intelliarts.conflab.core.entity.UserEntity;
 import com.intelliarts.conflab.core.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
 
 @Service
-public class UserService extends SimpleService<User, UserEntity> {
+public class UserService implements UserDetailsService {
+
     private final UserRepository repository;
 
     @Autowired
     public UserService(UserRepository repository) {
-        super(repository);
         this.repository = repository;
     }
 
-    public User getSessionUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return new User();
-        }
-
-        String username = principal instanceof UserDetails
-                ? ((UserDetails) principal).getUsername()
-                : principal.toString();
-
-        Optional<UserEntity> userEntity = repository.findByUsername(username);
-        return toApi(userEntity.orElseThrow(
-                () -> new EntityNotFoundException("User with username " + username + " was not found")));
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                         .orElseThrow(() -> new EntityNotFoundException(
+                                 String.format("User with username %s was not found", username)));
     }
+
 }
