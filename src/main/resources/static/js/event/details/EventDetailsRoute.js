eeb921c4965afcefaf7a81608f4abab7b6f099db
vitalii-dmytruk <vdmytruk@intelliarts.com>
2015-12-21 17:16:02 +0200

    [#109793900] show attached speeches on the event details page

--- src/main/resources/static/js/event/details/EventDetailsRoute.js
+++ src/main/resources/static/js/event/details/EventDetailsRoute.js
@@ -1,18 +1,52 @@
 define([
-   'common/route/ItemDetailsRoute',
+   'common/route/Route',
    'event/eventViewFactory',
    'speech/speechViewFactory',
    'speaker/speakerViewFactory',
    'event/EventModel',
-   'speaker/SpeakerCollection'
-], function (ItemDetailsRoute, eventViewFactory, speechViewFactory, speakerViewFactory,  EventModel, SpeakerCollection) {
+   'speaker/SpeakerCollection',
+   'speech/SpeechCollection'
+], function (Route, eventViewFactory, speechViewFactory, speakerViewFactory,  EventModel, SpeakerCollection, SpeechCollection) {
 
     'use strict';
 
-    return ItemDetailsRoute.extend({
-        itemModelClass             : EventModel,
-        attachedItemCollectionClass: SpeakerCollection,
-        itemDetailsView            : eventViewFactory.itemDetailsView,
-        attachedItemTableView      : speakerViewFactory.attachedItemTableView
+    return Route.extend({
+
+        initialize: function (options) {
+            this.container = options.container;
+        },
+
+        fetch: function (id) {
+            this.model            = new EventModel({id: id});
+
+            this.speakers       = new SpeakerCollection();
+            this.speakers.url   = this.model.url() + this.speakers.url;
+            this.speakersSearchCollection = new SpeakerCollection();
+
+            this.speeches       = new SpeechCollection();
+            this.speeches.url   = this.model.url() + this.speeches.url;
+            this.speechesSearchCollection = new SpeechCollection();
+
+            return $.when(this.model.fetch(),
+                          this.speeches.fetch(), this.speechesSearchCollection.fetch(),
+                          this.speakers.fetch(), this.speakersSearchCollection.fetch());
+        },
+
+        render: function () {
+            this.view = new eventViewFactory.itemDetailsView({
+                model     : this.model,
+                childViews: [
+                    new speakerViewFactory.attachedItemTableView({
+                        collection      : this.speakers,
+                        searchCollection: this.speakersSearchCollection
+                    }),
+                    new speechViewFactory.attachedItemTableView({
+                        collection      : this.speeches,
+                        searchCollection: this.speechesSearchCollection
+                    })
+                ]
+            });
+            this.container.show(this.view);
+        }
     });
 });
