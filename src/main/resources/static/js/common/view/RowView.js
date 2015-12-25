f039a73a77ceb9df74f450c58065d374df7a0d68
vitalii-dmytruk <vdmytruk@intelliarts.com>
2015-12-25 13:24:01 +0200

    [#110557206] show remove icon on the speaker row view

--- src/main/resources/static/js/common/view/RowView.js
+++ src/main/resources/static/js/common/view/RowView.js
@@ -7,13 +7,35 @@ define([
     return Marionette.ItemView.extend({
         tagName   : 'a',
         className : 'list-group-item',
+
+        ui : {
+            actionIcons: '[data-actions]'
+        },
+
+        events : {
+            'mouseenter': showActionIcons,
+            'mouseleave': hideActionIcons,
+            'hover @ui.actionIcons' : function(e){
+                return e.type;
+            }
+        },
+
         attributes: function () {
             return {
                 href: '#' + this.model.url()
             };
         },
+
         onRender  : function () {
             this.stickit();
         }
     });
+
+    function hideActionIcons(){
+        this.ui.actionIcons.hide(100);
+    }
+
+    function showActionIcons(){
+        this.ui.actionIcons.show(100);
+    }
 });
