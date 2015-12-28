7684824e8065087a80a0f327b94ccc98c3e63f44
approce <seateq64@gmail.com>
2015-12-28 11:39:09 +0200

    [#109879544] Move validation callback to application scope.

--- src/main/resources/static/js/common/view/EditView.js
+++ src/main/resources/static/js/common/view/EditView.js
@@ -9,23 +9,6 @@ define([
     return Marionette.ItemView.extend({
         initialize: function () {
             Backbone.Validation.bind(this);
-
-            _.extend(Backbone.Validation.callbacks, {
-                valid  : function (view, attr, selector) {
-                    var $el    = view.$('#' + attr),
-                        $group = $el.closest('.form-group');
-
-                    $group.removeClass('has-error');
-                    $group.find('.help-block').html('').addClass('hidden');
-                },
-                invalid: function (view, attr, error, selector) {
-                    var $el    = view.$('#' + attr),
-                        $group = $el.closest('.form-group');
-
-                    $group.addClass('has-error');
-                    $group.find('.help-block').html(error).removeClass('hidden');
-                }
-            });
         },
 
         ui: {
--- src/main/resources/static/js/conflab.js
+++ src/main/resources/static/js/conflab.js
@@ -9,4 +9,21 @@ require([
         new ConfLabApp().start();
     });
 
+    _.extend(Backbone.Validation.callbacks, {
+        valid  : function (view, attr, selector) {
+            var $el    = view.$('#' + attr),
+                $group = $el.closest('.form-group');
+
+            $group.removeClass('has-error');
+            $group.find('.help-block').html('').addClass('hidden');
+        },
+        invalid: function (view, attr, error, selector) {
+            var $el    = view.$('#' + attr),
+                $group = $el.closest('.form-group');
+
+            $group.addClass('has-error');
+            $group.find('.help-block').html(error).removeClass('hidden');
+        }
+    });
+
 });
