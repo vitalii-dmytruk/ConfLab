d53b30b7926f0bb754c0c68835fe2d0de8342dba
vitalii-dmytruk <vdmytruk@intelliarts.com>
2016-01-06 11:47:27 +0200

    CL-43 show edit form by double click on details view

--- src/main/resources/static/js/common/behavior/EditBehavior.js
+++ src/main/resources/static/js/common/behavior/EditBehavior.js
@@ -2,6 +2,10 @@ define([
     'common/behavior/ItemActionIconsBehavior'
 ], function EditBehavior(ActionIconsBehavior) {
     return ActionIconsBehavior.extend({
+        triggers : {
+            'dblclick' : 'edit'
+        },
+
         className: 'glyphicon-pencil edit',
         doAction : function destroyItem() {
             this.view.triggerMethod('edit');
