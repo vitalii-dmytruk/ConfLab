abc09c3b0494c05d92a743dcbbe7d8c0895e2ce0
approce <seateq64@gmail.com>
2016-01-06 14:37:16 +0200

    CL-46 implement creating/linking speeches in conference speaker view.

--- src/main/java/com/intelliarts/conflab/core/service/EventService.java
+++ src/main/java/com/intelliarts/conflab/core/service/EventService.java
@@ -1,6 +1,7 @@
 package com.intelliarts.conflab.core.service;
 
 import com.intelliarts.conflab.core.entity.Event;
+import com.intelliarts.conflab.core.entity.SpeechSpeaker;
 import com.intelliarts.conflab.core.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,14 @@ import java.util.Optional;
 @Service
 public class EventService {
 
+    private EventRepository           eventRepository;
+    private EventSpeechSpeakerService eventSpeechSpeakerService;
+
     @Autowired
-    private EventRepository eventRepository;
+    public EventService(EventRepository eventRepository, EventSpeechSpeakerService eventSpeechSpeakerService) {
+        this.eventRepository = eventRepository;
+        this.eventSpeechSpeakerService = eventSpeechSpeakerService;
+    }
 
     public List<Event> findAll() {
         return eventRepository.findAll();
@@ -26,5 +33,9 @@ public class EventService {
     public Event findById(Long id) {
         Optional<Event> event = eventRepository.findOne(id);
         return event.orElseThrow(() -> new EntityNotFoundException("Event with ID '" + id + "' " + "not found."));
+    }
+
+    public void link(Event event, SpeechSpeaker speechSpeaker) {
+        eventSpeechSpeakerService.createEventSpeechSpeakerLink(event, speechSpeaker);
     }
 }
