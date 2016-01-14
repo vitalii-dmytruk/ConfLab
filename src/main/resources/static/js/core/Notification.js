a3e6705ed3561817f00c72ff44a4814f40d0e06e
vitalii-dmytruk <vdmytruk@intelliarts.com>
2016-01-14 12:13:15 +0200

    CL-79 implement javascript notification

--- src/main/resources/static/js/core/Notification.js
+++ src/main/resources/static/js/core/Notification.js
@@ -1,10 +1,31 @@
 define([
-    'backbone'
-], function () {
+    'toastr'
+], function (toastr) {
     'use strict';
-    return function Notification(){
-        this.error = function (msg) {
-            alert(msg);
-        }
+
+    return function Notification() {
+        toastr.options = {
+            closeButton      : true,
+            iconClasses      : {
+                error  : 'notification-error',
+                info   : 'notification-info',
+                success: 'notification-success',
+                warning: 'notification-warning'
+            },
+            preventDuplicates: true
+        };
+
+        this.success = function (msg, title) {
+            toastr.success(msg, title || 'Success');
+        };
+        this.info    = function (msg, title) {
+            toastr.info(msg, title || 'Information');
+        };
+        this.warn    = function (msg, title) {
+            toastr.warning(msg, title || 'Warning');
+        };
+        this.error   = function (msg, title) {
+            toastr.error(msg, title || 'Error');
+        };
     };
 });
