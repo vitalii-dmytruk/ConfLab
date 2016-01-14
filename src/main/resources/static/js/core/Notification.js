0d46e0ac88ed326ee3cbb21e6240aa2bcbc6c8d2
vitalii-dmytruk <vdmytruk@intelliarts.com>
2016-01-14 14:10:57 +0200

    CL-79 code cleanup

--- src/main/resources/static/js/core/Notification.js
+++ src/main/resources/static/js/core/Notification.js
@@ -6,26 +6,34 @@ define([
     return function Notification() {
         toastr.options = {
             closeButton      : true,
+            preventDuplicates: true,
+            timeOut          : 3000,
             iconClasses      : {
                 error  : 'notification-error',
                 info   : 'notification-info',
                 success: 'notification-success',
                 warning: 'notification-warning'
-            },
-            preventDuplicates: true
+            }
         };
 
         this.success = function (msg, title) {
             toastr.success(msg, title || 'Success');
         };
-        this.info    = function (msg, title) {
+
+        this.info = function (msg, title) {
             toastr.info(msg, title || 'Information');
         };
-        this.warn    = function (msg, title) {
+
+        this.warn = function (msg, title) {
             toastr.warning(msg, title || 'Warning');
         };
-        this.error   = function (msg, title) {
+
+        this.error = function (msg, title) {
             toastr.error(msg, title || 'Error');
         };
+
+        this.clear = function () {
+            toastr.clear();
+        }
     };
 });
