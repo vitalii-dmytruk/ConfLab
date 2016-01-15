132d61104476b69668ad60617ea6df6b28917285
vitalii-dmytruk <vdmytruk@intelliarts.com>
2016-01-15 15:44:33 +0200

    CL-86 add json response to authentication errors

---
+++ src/main/java/com/intelliarts/conflab/security/AuthenticationFailedEntryPoint.java
@@ -0,0 +1,24 @@
+package com.intelliarts.conflab.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationFailedEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        Throwable ex = authException;
+        while (ex.getCause() != null) {
+            ex = ex.getCause();
+        }
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+    }
+}
