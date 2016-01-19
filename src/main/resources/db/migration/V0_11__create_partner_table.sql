81f23bcb7aacc68d19a251fc1b0fce0416220367
Igor Rudnyk <irudnyk@intelliarts.com>
2016-01-19 12:47:25 +0200

    CL-90 create table 'partner'

---
+++ src/main/resources/db/migration/V0_11__create_partner_table.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE partner_id_seq;
+
+CREATE TABLE partner (
+  id   BIGINT       NOT NULL PRIMARY KEY DEFAULT nextval('partner_id_seq'),
+  name VARCHAR(255) NOT NULL,
+  link VARCHAR
+);
+
+CREATE TABLE partner_event (
+  event_id BIGINT NOT NULL,
+  partner_id BIGINT NOT NULL,
+  FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
+  FOREIGN KEY (partner_id) REFERENCES partner (id) ON DELETE CASCADE
+);
