23e184df0359593b24b18d40f8a942ca21171e6a
approce <seateq64@gmail.com>
2016-01-20 17:19:53 +0200

    CL-58 Remove redundant image repository/service/controller.

--- src/main/java/com/intelliarts/conflab/core/repository/ImagesRepository.java
+++ src/main/java/com/intelliarts/conflab/core/repository/ImagesRepository.java
@@ -2,24 +2,16 @@ package com.intelliarts.conflab.core.repository;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Component
 public class ImagesRepository {
 
     @Value("${images.path}")
-    private String imagesFolder;
+    private String rootPath;
 
-    public File get(String id) throws FileNotFoundException {
-        String path = "images" + "/" + id + ".jpg";
-        File file = new File(path);
-        if (file.exists() && file.canRead() && file.isFile()) {
-            return file;
-        } else {
-            throw new FileNotFoundException(
-                    String.format("Image with identifier '%s' was not found in path '%s'", id, imagesFolder));
-        }
-    }
 }
