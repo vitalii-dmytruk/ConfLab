aae2f3a2b77839ff343e22c39cbdf6a481eff098
irudnyk <irudnyk@intelliarts.com>
2016-01-24 23:22:20 +0200

    CL-91 UI add partner level support

--- src/main/resources/static/js/event/details/partners/PartnerLevelRoute.js
+++ src/main/resources/static/js/event/details/partners/PartnerLevelRoute.js
@@ -1,9 +1,30 @@
 define([
-    'common/route/Route'
-], function (Route) {
+    'common/route/Route',
+    'event/details/partners/PartnerLevelModel',
+    'event/details/partners/PartnerLevelCollection',
+    'event/details/partners/PartnerLevelView',
+    'backbone'
+], function (Route, PartnerLevelModel, PartnerLevelCollection, PartnerLevelView) {
 
     'use strict';
 
-    return Route.extend({});
+    return Route.extend({
+
+        fetch: function (container, eventModel, itemModel) {
+            this.urlRoot       = eventModel.url() + itemModel.url();
+            this.model         = new PartnerLevelModel();
+            this.model.urlRoot = this.urlRoot + this.model.urlRoot;
+            this.collection    = new PartnerLevelCollection();
+            return $.when(this.model.fetch(), this.collection.fetch());
+        },
+
+        render: function (container) {
+            container.show(new PartnerLevelView({
+                model     : new Backbone.Model({partnerLevel: this.model.attributes}),
+                collection: this.collection,
+                urlRoot   : this.urlRoot
+            }));
+        }
+    });
 
 });
