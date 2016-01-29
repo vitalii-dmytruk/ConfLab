6c2a7d45556a9290d7b053ddc967543a92f05bb4
Igor Rudnyk <irudnyk@intelliarts.com>
2016-01-29 19:56:42 +0200

    CL-58 extract image edit view

--- src/main/resources/static/js/speaker/SpeakerModel.js
+++ src/main/resources/static/js/speaker/SpeakerModel.js
@@ -16,7 +16,7 @@ define([
                 about   : '',
                 email   : '',
                 company : null,
-                image   : null
+                image   : '/img/default-avatar.png'
             }
         },
         validation: {
@@ -57,7 +57,7 @@ define([
             fileData = model.image;
 
         if (fileData) {
-            formData.append('image', dataURItoBlob(fileData), fileName);
+            formData.append('image', fileData, fileName);
         }
 
         formData.append('speaker', new Blob([JSON.stringify(model.toJSON())], {
@@ -67,24 +67,4 @@ define([
         return formData;
     }
 
-    function dataURItoBlob(dataURI) {
-        // convert base64/URLEncoded data component to raw binary data held in a string
-        var byteString;
-        if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-            byteString = atob(dataURI.split(',')[1]);
-        } else {
-            byteString = unescape(dataURI.split(',')[1]);
-        }
-
-        // separate out the mime component
-        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-        // write the bytes of the string to a typed array
-        var ia = new Uint8Array(byteString.length);
-        for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-        }
-
-        return new Blob([ia], {type: mimeString});
-    }
 });
--- src/main/resources/static/js/speaker/details/SpeakerEditView.js
+++ src/main/resources/static/js/speaker/details/SpeakerEditView.js
@@ -1,79 +1,37 @@
 define([
-    'common/view/EditView',
-    'croppie'
-], function (EditView) {
+    'common/behavior/FormBehavior',
+    'common/image/ImageEditView',
+    'backbone.marionette'
+], function (FormBehavior, ImageEditView) {
 
     'use strict';
 
-
-    return EditView.extend({
-        ui: {
-            croppie        : '#croppie',
-            avatarImg:'#avatar-img',
-            avatarDiv      : '#avatar-div',
-            croppieDiv     : '#croppie-div',
-            uploadAvatarBtn: '#upload-avatar-button',
-            submitAvatarBtn: '#croppie-submit'
+    return Marionette.LayoutView.extend({
+        behaviors: {
+            form: {
+                behaviorClass: FormBehavior
+            }
         },
 
-        events: {
-            'change @ui.uploadAvatarBtn': showImageEditView,
-            'click @ui.submitAvatarBtn' : addImageToModel
+        regions  : {
+            avatarRegion: '#avatar'
+        },
+
+        onBeforeShow: function () {
+            new ImageEditView({
+                container   : this.avatarRegion,
+                model       : this.model,
+                imageUrlAttr: 'image',
+                viewport    : {
+                    width : 200,
+                    height: 200,
+                    type  : 'circle'
+                },
+                boundary    : {
+                    width : 200,
+                    height: 200
+                }
+            }).show();
         }
     });
-
-    function addImageToModel() {
-        var view = this;
-
-        this.$croppie.croppie('result', {
-            type: 'canvas',
-            size: 'original'
-        }).then(function (resp) {
-            view.ui.avatarImg.attr('src', resp);
-            view.model.image = resp;
-            toggleImgView(view);
-        });
-    }
-
-    function showImageEditView(e) {
-        initCroppie(this);
-        readFile(e.target.files, this.$croppie);
-        toggleImgView(this);
-    }
-
-    function initCroppie(view) {
-        view.$croppie = view.ui.croppie.croppie({
-            viewport: {
-                width : 150,
-                height: 150,
-                type  : 'circle'
-            },
-            boundary: {
-                width : 200,
-                height: 200
-            }
-        });
-    }
-
-    function readFile(files, $croppie) {
-        var file = files && files[0];
-
-        if (file) {
-            var reader    = new FileReader();
-            reader.onload = function (e) {
-                $croppie.croppie('bind', {
-                    url: e.target.result
-                });
-            };
-            reader.readAsDataURL(file);
-        }
-        else {
-            swal("Sorry - you're browser doesn't support the FileReader API");
-        }
-    }
-
-    function toggleImgView(view) {
-        view.ui.avatarDiv.toggle();
-        view.ui.croppieDiv.toggle();
-    }
 });
