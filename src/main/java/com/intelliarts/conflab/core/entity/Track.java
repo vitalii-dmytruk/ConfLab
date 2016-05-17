ab571da464d49e317be4a802a361321da94bf2c3
approce <seateq64@gmail.com>
2016-05-17 15:36:21 +0300

    CL-110 Create relationship between event and track.

--- src/main/java/com/intelliarts/conflab/core/entity/Track.java
+++ src/main/java/com/intelliarts/conflab/core/entity/Track.java
@@ -1,10 +1,13 @@
 package com.intelliarts.conflab.core.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,11 +20,16 @@ public class Track extends AbstractPersistable<Integer> {
     private String name;
 
     @Column
-    private int capacity;
+    private int   capacity;
 
     public String getName() {
         return name;
     }
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    @JsonIgnore
+    private Event event;
 
     public void setName(String name) {
         this.name = name;
@@ -33,5 +41,13 @@ public class Track extends AbstractPersistable<Integer> {
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
     }
 }
