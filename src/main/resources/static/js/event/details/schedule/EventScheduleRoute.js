c3ec57776862bef54e2aedc8874e7bc38a0945b7
approce <seateq64@gmail.com>
2016-06-10 14:35:48 +0300

    CL-131 remove redundant view. Use layout view instead.

--- src/main/resources/static/js/event/details/schedule/EventScheduleRoute.js
+++ src/main/resources/static/js/event/details/schedule/EventScheduleRoute.js
@@ -1,30 +1,52 @@
 define([
     'common/route/Route',
-    'event/details/schedule/EventScheduleView',
-    'track/TracksCollection'
-], function EventScheduleRoute(Route, EventScheduleView, TracksCollection) {
+    'event/details/schedule/view/ScheduleLayoutView',
+    'event/details/schedule/view/GridstackView',
+    'text!event/details/schedule/template/SpeechScheduleTemplate.html',
+    'track/TracksCollection',
+    'speech/SpeechCollection'
+], function EventScheduleRoute(Route, ScheduleLayoutView, GridstackView, SpeechScheduleTemplate, TracksCollection,
+                               SpeechCollection) {
 
     'use strict';
 
     return Route.extend({
 
         initialize: function (options) {
-            this.event      = options.event;
-            this.container  = options.container;
-            this.tracks     = new TracksCollection();
-            this.tracks.url = this.event.url() + this.tracks.url;
+            this.event     = options.event;
+            this.container = options.container;
+
+            this.tracks       = new TracksCollection();
+            this.tracks.url   = this.event.url() + this.tracks.url;
+            this.speeches     = new SpeechCollection();
+            this.speeches.url = this.event.url() + this.speeches.url;
         },
 
         fetch: function () {
-            return this.tracks.fetch();
+            return $.when(this.tracks.fetch(), this.speeches.fetch());
         },
 
         render: function () {
-            var eventScheduleView = new EventScheduleView({
-                model : this.event,
-                tracks: this.tracks
-            });
-            this.container.show(eventScheduleView);
+            var scheduleLayoutView = new ScheduleLayoutView(),
+                scheduleView       = new GridstackView({
+                    float    : true,
+                    rowsCount: 8,
+                    width    : this.tracks.length
+                }),
+                speechesView       = new GridstackView({
+                    width: 1
+                });
+
+            this.container.show(scheduleLayoutView);
+            scheduleLayoutView.showChildView('schedule', scheduleView);
+            scheduleLayoutView.showChildView('speeches', speechesView);
+            showSpeeches(this.speeches, speechesView);
         }
     });
+
+    function showSpeeches(speeches, speechView) {
+        speeches.each(function (speech, i) {
+            speechView.addWidget($(_.template(SpeechScheduleTemplate)({title: speech.get('title')})), 0, i, 1, 1);
+        });
+    }
 });
