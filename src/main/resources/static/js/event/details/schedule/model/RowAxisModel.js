c3710a2dab8e70102d69d8872878067b2bab160a
approce <seateq64@gmail.com>
2016-06-14 19:22:33 +0300

    CL-133 create models & views for grid axis.

---
+++ src/main/resources/static/js/event/details/schedule/model/RowAxisModel.js
@@ -0,0 +1,13 @@
+define([
+    'backbone'
+], function RowAxisModel() {
+
+    'use strict';
+
+    return Backbone.Model.extend({
+
+        defaults: {
+            time: null
+        }
+    });
+});
